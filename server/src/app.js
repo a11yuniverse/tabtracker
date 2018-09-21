7b4ad471602a973e0901a7886db68c43c370e090
JohnSweet <a11yexpert@gmail.com>
2018-09-21 11:50:15 -0600

    Fixed 8081 get issue

--- server/src/app.js
+++ server/src/app.js
@@ -8,6 +8,12 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world!'
+  })
+})
+
 app.post('/register', (req, res) => {
   res.send({
     message: `Hello ${req.body.email}! Your user was registered! Have fun!`
